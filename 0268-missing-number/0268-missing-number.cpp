9e6ea218212e741dce68e0f3cacd62ace92f9844
Madanmohan-Rana <161158827+Madanmohan-Rana@users.noreply.github.com>
2024-02-25 13:25:06 +0530

    Time: 14 ms (57.47%), Space: 20.2 MB (85.11%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,18 +2,28 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         //with mapping -> t.c=O(n)+O(n)     s.c=O(n)
-        unordered_map<int,int>mpp;
+        // unordered_map<int,int>mpp;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     mpp[nums[i]]++;
+        // }
+        // for(int i=0;i<=nums.size();i++)
+        // {
+        //     if(mpp.find(i)==mpp.end())
+        //     {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        
+        //using sum -> t.c=O(n) s.c=O(1)
+        int n=nums.size();
+        int total_sum=(n*(n+1))/2;
+        int array_sum=0;
         for(int i=0;i<nums.size();i++)
         {
-            mpp[nums[i]]++;
+            array_sum+=nums[i];
         }
-        for(int i=0;i<=nums.size();i++)
-        {
-            if(mpp.find(i)==mpp.end())
-            {
-                return i;
-            }
-        }
-        return -1;
+        return (total_sum-array_sum);
     }
 };
